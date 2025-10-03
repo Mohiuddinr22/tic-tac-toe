f93ae230459c8341f49b5a549ab38f7d687651ae
Mohiuddin Mohi <mohiuddinr.22@gmail.com>
2025-10-03 06:50:15 +0600

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import "../index.css";
+import { useState } from "react";
+
+export default function Player({initialName, symbol, isActive, onChangeName}){
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick(){
+        setIsEditing(editing => !editing);
+        
+        if(isEditing){
+            onChangeName(symbol, playerName);
+        }
+    }
+
+    const handleNameChange = (e) => {
+        setPlayerName(e.target.value);
+    }
+
+    return (
+        <li className={isActive ? "active" : undefined}>
+            <span className="player">
+                {!isEditing ? <span className="player-name">{playerName}</span>
+                            : <input type="text" value={playerName} required onChange={handleNameChange}/>}
+                
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? "save" : "edit"}</button>
+        </li>
+    );
+}
